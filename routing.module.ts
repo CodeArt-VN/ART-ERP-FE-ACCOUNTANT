3bf0c080a1e6682703e895fefa8e6415007751d7
Hùng Vũ <hungvq@live.com>
2024-05-28 09:04:44 +0700

    clean

--- routing.module.ts
+++ routing.module.ts
@@ -4,16 +4,16 @@ import { AuthGuard } from 'src/app/guards/app.guard';
 export const ACCOUNTTANTRoutes: Routes = [
   
   //ACCOUNTANT
-  { path: 'arinvoice', loadChildren: () => import('./arinvoice/arinvoice.module').then(m => m.ARInvoicePageModule), canActivate: [AuthGuard] },
-  { path: 'arinvoice/:id', loadChildren: () => import('./ar-invoice-detail/ar-invoice-detail.module').then(m => m.ARInvoiceDetailPageModule), canActivate: [AuthGuard] },
+  // { path: 'arinvoice', loadChildren: () => import('./arinvoice/arinvoice.module').then(m => m.ARInvoicePageModule), canActivate: [AuthGuard] },
+  // { path: 'arinvoice/:id', loadChildren: () => import('./ar-invoice-detail/ar-invoice-detail.module').then(m => m.ARInvoiceDetailPageModule), canActivate: [AuthGuard] },
 
-  { path: 'ar-invoice', loadChildren: () => import('./arinvoice/arinvoice.module').then(m => m.ARInvoicePageModule), canActivate: [AuthGuard] },
-  { path: 'ar-invoice/:id', loadChildren: () => import('./ar-invoice-detail/ar-invoice-detail.module').then(m => m.ARInvoiceDetailPageModule), canActivate: [AuthGuard] },
+  // { path: 'ar-invoice', loadChildren: () => import('./arinvoice/arinvoice.module').then(m => m.ARInvoicePageModule), canActivate: [AuthGuard] },
+  // { path: 'ar-invoice/:id', loadChildren: () => import('./ar-invoice-detail/ar-invoice-detail.module').then(m => m.ARInvoiceDetailPageModule), canActivate: [AuthGuard] },
 
-  { path: 'ap-invoice', loadChildren: () => import('./ap-invoice/ap-invoice.module').then(m => m.APInvoicePageModule), canActivate: [AuthGuard] },
-  { path: 'ap-invoice/:id', loadChildren: () => import('./ap-invoice-detail/ap-invoice-detail.module').then(m => m.APInvoiceDetailPageModule), canActivate: [AuthGuard] },
+  // { path: 'ap-invoice', loadChildren: () => import('./ap-invoice/ap-invoice.module').then(m => m.APInvoicePageModule), canActivate: [AuthGuard] },
+  // { path: 'ap-invoice/:id', loadChildren: () => import('./ap-invoice-detail/ap-invoice-detail.module').then(m => m.APInvoiceDetailPageModule), canActivate: [AuthGuard] },
 
-  { path: 'incoming-payment', loadChildren: () => import('./incoming-payment/incoming-payment.module').then(m => m.IncomingPaymentPageModule), canActivate: [AuthGuard] },
-  { path: 'incoming-payment/:id', loadChildren: () => import('./incoming-payment-detail/incoming-payment-detail.module').then(m => m.IncomingPaymentDetailPageModule), canActivate: [AuthGuard] },
+  // { path: 'incoming-payment', loadChildren: () => import('./incoming-payment/incoming-payment.module').then(m => m.IncomingPaymentPageModule), canActivate: [AuthGuard] },
+  // { path: 'incoming-payment/:id', loadChildren: () => import('./incoming-payment-detail/incoming-payment-detail.module').then(m => m.IncomingPaymentDetailPageModule), canActivate: [AuthGuard] },
 
 ];
